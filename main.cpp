7ba7e89f55befe859843c1cd8a9c827ef3f819e1
eXhumer <eXhumer@member.fsf.org>
2021-01-08 12:47:26 -0700

    Configure app_config.h > Set application name/version from configured app_config.h

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "AppWidgets/mainwindow.h"
-
+#include "app_config.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    app.setApplicationName(PROJECT_NAME);
+    app.setApplicationVersion(PROJECT_VERSION);
     MainWindow win;
     win.show();
     return app.exec();
