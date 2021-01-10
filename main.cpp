94f6b055d67848b6f87cf7b63441a16c4131a303
eXhumer <exhumer@member.fsf.org>
2021-01-10 02:19:07 -0700

    Add translation files to output binary

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include "AppWidgets/mainwindow.h"
 #include "app_config.h"
 #include <QApplication>
+#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     app.setApplicationName(PROJECT_NAME);
     app.setApplicationVersion(PROJECT_VERSION);
+    QTranslator appTranslator;
+    if(appTranslator.load(QLocale(), PROJECT_NAME, ".", QString(), ".qm"))
+        app.installTranslator(&appTranslator);
     MainWindow win;
     win.show();
     return app.exec();
