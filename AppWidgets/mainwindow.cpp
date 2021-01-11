81183493f7d0c4108dcf30aeee1e5160b3446db3
eXhumer <exhumer@member.fsf.org>
2021-01-11 01:17:53 -0700

    Better license notice

--- AppWidgets/mainwindow.cpp
+++ AppWidgets/mainwindow.cpp
@@ -1,19 +1,23 @@
-/*
-    TinGen - Index Generator for Tinfoil
-    Copyright (C) 2021 eXhumer
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
+/***************************************************************************
+**
+** TinGen - Index Generator for Tinfoil
+** Copyright (C) 2021 eXhumer
+**
+** $QT_BEGIN_LICENSE:GPL3$
+** GNU General Public License Usage
+** This file may be used under the terms of the GNU General Public License
+** version 3. The licenses are as published by the Free Software Foundation
+** and appearing in the file LICENSE.GPL3 included in the packaging of this
+** file. Please review the following information to ensure the GNU General
+** Public License requirements will be met:
+** https://www.gnu.org/licenses/gpl-3.0.html.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program.  If not, see <https://www.gnu.org/licenses/>.
+**
+** $QT_END_LICENSE$
+**
+***************************************************************************/
 
 #include "centralwidget.h"
 #include "mainwindow.h"
