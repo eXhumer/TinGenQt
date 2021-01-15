3fdf0a4105774e54236b771145b13ad19b8fb5bb
eXhumer <exhumer@member.fsf.org>
2021-01-15 15:20:49 -0700

    Add Google FS Model to AppWidgets

---
+++ AppWidgets/googlefilesystemmodel.cpp
@@ -0,0 +1,82 @@
+#include "googlefilesystemmodel.h"
+#include <QIcon>
+
+QString GoogleFileSystemModel::fileID(const QModelIndex &index) const
+{
+    // TODO: FIXME
+}
+
+QString GoogleFileSystemModel::fileName(const QModelIndex &index) const
+{
+    // TODO: FIXME
+}
+
+GoogleFileSystemModel::FileType GoogleFileSystemModel::fileType(const QModelIndex &index) const
+{
+    // TODO: FIXME
+}
+
+QIcon GoogleFileSystemModel::fileIcon(const QModelIndex &index) const
+{
+    // TODO: FIXME
+}
+
+bool GoogleFileSystemModel::isDir(const QModelIndex &index) const
+{
+    // TODO: FIXME
+}
+
+bool GoogleFileSystemModel::selected(const QModelIndex &index) const
+{
+    // TODO: FIXME
+}
+
+GoogleFileSystemModel::GoogleFileSystemModel(QObject *parent)
+{
+    // TODO: FIXME
+}
+
+QVariant GoogleFileSystemModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    // TODO: FIXME
+}
+
+QModelIndex GoogleFileSystemModel::index(int row, int column, const QModelIndex &parent) const
+{
+    // TODO: FIXME
+}
+
+QModelIndex GoogleFileSystemModel::parent(const QModelIndex &index) const
+{
+    // TODO: FIXME
+}
+
+int GoogleFileSystemModel::rowCount(const QModelIndex &parent) const
+{
+    // TODO: FIXME
+}
+
+int GoogleFileSystemModel::columnCount(const QModelIndex &parent) const
+{
+    // TODO: FIXME
+}
+
+bool GoogleFileSystemModel::hasChildren(const QModelIndex &parent) const
+{
+    // TODO: FIXME
+}
+
+bool GoogleFileSystemModel::canFetchMore(const QModelIndex &parent) const
+{
+    // TODO: FIXME
+}
+
+void GoogleFileSystemModel::fetchMore(const QModelIndex &parent)
+{
+    // TODO: FIXME
+}
+
+QVariant GoogleFileSystemModel::data(const QModelIndex &index, int role) const
+{
+    // TODO: FIXME
+}
