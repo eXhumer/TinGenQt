bec1f1c4601e94aa752c305a7bd85d980e8659b9
eXhumer <exhumer@member.fsf.org>
2021-01-15 07:04:53 -0700

    Add license to source

--- AppWidgets/googlefolderselectormodel.cpp
+++ AppWidgets/googlefolderselectormodel.cpp
@@ -1,3 +1,24 @@
+/***************************************************************************
+**
+** TinGen - Index Generator for Tinfoil
+** Copyright (C) 2021 eXhumer
+**
+** $QT_BEGIN_LICENSE:GPL3$
+** GNU General Public License Usage
+** This file may be used under the terms of the GNU General Public License
+** version 3. The licenses are as published by the Free Software Foundation
+** and appearing in the file LICENSE.GPL3 included in the packaging of this
+** file. Please review the following information to ensure the GNU General
+** Public License requirements will be met:
+** https://www.gnu.org/licenses/gpl-3.0.html.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program.  If not, see <https://www.gnu.org/licenses/>.
+**
+** $QT_END_LICENSE$
+**
+***************************************************************************/
+
 #include "googlefolderselectormodel.h"
 
 GoogleFolderSelectorModel::GoogleFolderSelectorModel(QObject *parent)
