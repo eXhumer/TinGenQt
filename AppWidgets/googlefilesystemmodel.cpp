57331db3687d8bb50142c7dce0913ed77fca3323
eXhumer <exhumer@member.fsf.org>
2021-01-18 21:23:47 -0700

    Initial implementation of some functions

--- AppWidgets/googlefilesystemmodel.cpp
+++ AppWidgets/googlefilesystemmodel.cpp
@@ -4,32 +4,35 @@
 GoogleFileSystemModel::GoogleFileSystemModel(QObject *parent)
     : QAbstractItemModel(parent)
 {
-    // TODO: FIXME
+    this->rootItem = new GoogleFileSystemItem({
+        {{Qt::DisplayRole, "Test 1"}},
+        {{Qt::DisplayRole, "Test 2"}},
+    }, nullptr);
 }
 
 QString GoogleFileSystemModel::fileID(const QModelIndex &index) const
 {
-    // TODO: FIXME
+    return this->data(index, GoogleFileSystemModel::FileIDRole).toString();
 }
 
 QString GoogleFileSystemModel::fileName(const QModelIndex &index) const
 {
-    // TODO: FIXME
+    return this->data(index, GoogleFileSystemModel::FileNameRole).toString();
 }
 
 GoogleFileSystemModel::FileType GoogleFileSystemModel::fileType(const QModelIndex &index) const
 {
-    // TODO: FIXME
+    return this->data(index, GoogleFileSystemModel::FileTypeRole).value<GoogleFileSystemModel::FileType>();
 }
 
 QIcon GoogleFileSystemModel::fileIcon(const QModelIndex &index) const
 {
-    // TODO: FIXME
+    return this->data(index, GoogleFileSystemModel::FileIconRole).value<QIcon>();
 }
 
 bool GoogleFileSystemModel::isDir(const QModelIndex &index) const
 {
-    // TODO: FIXME
+    return this->fileType(index) == GoogleFileSystemModel::Folder || this->fileType(index) == GoogleFileSystemModel::Drive;
 }
 
 GoogleFileSystemModel::SelectState GoogleFileSystemModel::selectState(const QModelIndex &index) const
